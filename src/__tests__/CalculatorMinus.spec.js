add3dfbb1e51671c28520323ae935e79f3f93d48
isaac_lee <yy0221ss@gmail.com>
2022-05-02 18:22:26 +0900

    typo change

--- src/__tests__/CalculatorMinus.spec.js
+++ src/__tests__/CalculatorMinus.spec.js
@@ -1,6 +1,6 @@
 import Calculator from "../js/calculator/Calculator";
 
-describe("사직연산 덧셈 테스트.", () => {
+describe("사직연산 뺄셈 테스트.", () => {
   test("3과 5를 빼면 -2이다.", () => {
     const calc = new Calculator();
     expect(calc.minus(3, 5)).toEqual(-2);
